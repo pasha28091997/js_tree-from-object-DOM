94fc53fa255b1153c1b708807d5f22f01c02b49a
Pasha Shchukin <pasha4320692@gmail.com>
2024-10-27 18:30:25 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -32,7 +32,7 @@ function createTree(element, data) {
 
     li.textContent = key;
 
-    if (typeof data[key] === 'object') {
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) {
       createTree(li, data[key]);
     }
 
